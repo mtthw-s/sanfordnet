8b508f926bb6aba88b4dc9f110c5fea014f60216
Ubuntu <ubuntu@ip-172-31-16-228.us-west-2.compute.internal>
2019-04-05 23:35:34 +0000

    Inital commit

---
+++ index.js
@@ -0,0 +1,35 @@
+//Get dependancies
+const express = require('express');
+const path = require('path');
+const http = require('http');
+const bodyParser = require('body-parser');
+
+//Get API routes
+const api = require('./server/routes/api.js');
+
+const app = express();
+
+//parsers for post data
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+//point to static path to dist
+app.use(express.static(path.join(__dirname, 'dist/sanfordweb')));
+
+//set api routes
+app.use('/api', api);
+
+//return root file for all other routes
+app.get('*', (req, res) =>{
+    res.sendFile(path.join(__dirname, 'dist/sanfordweb/index.html'));
+});
+
+const port = process.env.PORT || '8080';
+app.set('port', port);
+
+//Create http server
+const server = http.createServer(app);
+
+server.listen(port, () => {
+    console.log(`API running on localhost:${port}`);
+});
